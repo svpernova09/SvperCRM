0e302c580d6de057c49eae21587f4ba2eca96137
Svpernova09 <joe@joeferguson.me>
2014-09-03 08:46:54 -0500

    reformat return statements

--- app/models/Organization.php
+++ app/models/Organization.php
@@ -5,21 +5,25 @@ class Organization extends \Eloquent {
 
     public function credentials()
     {
+
         return $this->hasMany('Credential');
     }
 
     public function contacts()
     {
+
         return $this->hasMany('Person');
     }
 
     public function salesPerson()
     {
+
         return $this->hasOne('Person', 'id', 'salesperson_id');
     }
 
     public function accountManager()
     {
+
         return $this->hasOne('Person', 'id', 'account_manager_id');
     }
 }
