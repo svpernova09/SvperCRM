b477b1b74241410bf1fd9276cd2644a5834512bd
Svpernova09 <joe@joeferguson.me>
2014-09-03 08:45:14 -0500

    Adding relationships to organization

--- app/models/Organization.php
+++ app/models/Organization.php
@@ -2,4 +2,24 @@
 
 class Organization extends \Eloquent {
 	protected $fillable = [];
+
+    public function credentials()
+    {
+        return $this->hasMany('Credential');
+    }
+
+    public function contacts()
+    {
+        return $this->hasMany('Person');
+    }
+
+    public function salesPerson()
+    {
+        return $this->hasOne('Person', 'id', 'salesperson_id');
+    }
+
+    public function accountManager()
+    {
+        return $this->hasOne('Person', 'id', 'account_manager_id');
+    }
 }
