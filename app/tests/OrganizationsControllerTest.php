c13f4a2b578317b1e2406d6b42937bb00ad680bb
Svpernova09 <joe@joeferguson.me>
2014-09-04 07:36:02 -0500

    Back to test basics

--- app/tests/OrganizationsControllerTest.php
+++ app/tests/OrganizationsControllerTest.php
@@ -1,31 +1,30 @@
 <?php
-//
-//
-//class OrganizationsControllerTest extends TestCase {
-//
-//    public function __construct()
-//    {
-//        parent::setUp();
-//        // We have no interest in testing Eloquent
-//        $this->mock = Mockery::mock('Eloquent', 'Organization');
-//    }
-//
-//    public function tearDown()
-//    {
-//        Mockery::close();
-//    }
-//
-//    public function testIndex()
-//    {
-//        $this->mock
-//            ->shouldReceive('all')
-//            ->once()
-//            ->andReturn('foo');
-//
-//        $this->app->instance('Organization', $this->mock);
-//
-//        $this->call('GET', 'organizations');
-//
-//        $this->assertViewHas('organizations');
-//    }
-//}
+
+class OrganizationsControllerTest extends TestCase {
+
+    public function tearDown()
+    {
+        Mockery::close();
+    }
+
+    public function __construct()
+    {
+        parent::setUp();
+
+        $this->mock = Mockery::mock('Eloquent', 'Organization');
+    }
+
+    public function testIndex()
+    {
+        $this->mock
+            ->shouldReceive('all')
+            ->once()
+            ->andReturn('foo');
+
+        $this->app->instance('Organization', $this->mock);
+
+        $this->call('GET', '/organizations');
+
+        $this->assertViewHas('organizations');
+    }
+}
--- tests/functional/OrganizationsCept.php
+++
@@ -1,8 +0,0 @@
-<?php 
-$I = new TestGuy($scenario);
-$I->am('a user');
-$I->wantTo('view the organizations index');
-
-$I->amOnRoute('organizations.index');
-
-$I->canSeeRecord('organizations');
