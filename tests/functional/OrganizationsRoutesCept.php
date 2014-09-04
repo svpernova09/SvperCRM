74d44681a9dee3a238c5770485f72402dd802251
Svpernova09 <joe@joeferguson.me>
2014-09-03 23:11:03 -0500

    Adding test cases

--- tests/functional/OrganizationsRoutesCept.php
+++ tests/functional/OrganizationsRoutesCept.php
@@ -3,6 +3,7 @@ $I = new TestGuy($scenario);
 $I->am('a user');
 $I->wantTo('test the organizations routes');
 
+// index
 $I->amOnRoute('organizations.index');
 $I->seeCurrentUrlEquals('/organizations');
 $I->seeCurrentActionIs('OrganizationsController@index');
@@ -10,3 +11,12 @@ $I->seeCurrentActionIs('OrganizationsController@index');
 $I->amOnAction('OrganizationsController@index');
 $I->seeCurrentUrlEquals('/organizations');
 $I->seeCurrentRouteIs('organizations.index');
+
+//create
+$I->amOnRoute('organizations.create');
+$I->seeCurrentUrlEquals('/organizations/create');
+$I->seeCurrentActionIs('OrganizationsController@create');
+
+$I->amOnAction('OrganizationsController@create');
+$I->seeCurrentUrlEquals('/organizations/create');
+$I->seeCurrentRouteIs('organizations.create');
