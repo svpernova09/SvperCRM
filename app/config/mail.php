d1a929053890536cf09ee5306160b3477e9079c2
Svpernova09 <joe@joeferguson.me>
2014-09-07 15:04:38 -0500

    Add outgoing mail from info

--- app/config/mail.php
+++ app/config/mail.php
@@ -54,7 +54,7 @@ return array(
 	|
 	*/
 
-	'from' => array('address' => null, 'name' => null),
+	'from' => array('address' => 'joe@hackmemphis.com', 'name' => 'SvperCRM'),
 
 	/*
 	|--------------------------------------------------------------------------
