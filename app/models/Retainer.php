6c29ca3c2085193bcf280e86247af91d3e04d029
Svpernova09 <joe@joeferguson.me>
2014-09-08 08:36:30 -0500

    Set fillables on retainer model

--- app/models/Retainer.php
+++ app/models/Retainer.php
@@ -1,7 +1,14 @@
 <?php
 
 class Retainer extends \Eloquent {
-	protected $fillable = [];
+	protected $fillable = [
+        'title',
+        'hours',
+        'strategiest_id',
+        'account_manager_id',
+        'domain',
+        'comments'
+    ];
 
     public function organization()
     {
