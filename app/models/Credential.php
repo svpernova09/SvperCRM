92cb3d3c5d0e71909f9b67a5abc4b59aed446dae
Svpernova09 <joe@joeferguson.me>
2014-09-11 08:48:17 -0500

    Store credential data encrypted and decrypt for display

--- app/models/Credential.php
+++ app/models/Credential.php
@@ -14,4 +14,36 @@ class Credential extends \Eloquent {
 
         return $this->belongsTo('Organization');
     }
+
+    public function setServiceNameAttribute($value) {
+        $this->attributes['service_name'] = Crypt::encrypt($value);
+    }
+
+    public function getServiceNameAttribute($value){
+        return Crypt::decrypt($value);
+    }
+
+    public function setCommentsAttribute($value) {
+        $this->attributes['comments'] = Crypt::encrypt($value);
+    }
+
+    public function getCommentsAttribute($value){
+        return Crypt::decrypt($value);
+    }
+
+    public function setPasswordAttribute($value) {
+        $this->attributes['password'] = Crypt::encrypt($value);
+    }
+
+    public function getPasswordAttribute($value){
+        return Crypt::decrypt($value);
+    }
+
+    public function setUserNameAttribute($value) {
+        $this->attributes['user_name'] = Crypt::encrypt($value);
+    }
+
+    public function getUserNameAttribute($value){
+        return Crypt::decrypt($value);
+    }
 }
