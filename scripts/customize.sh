38eb5efb324c42e988b9bb9d7bb128a7864b5dff
Svpernova09 <joe@joeferguson.me>
2014-09-14 12:31:50 -0500

    Add homestead vagrant box to project.

---
+++ scripts/customize.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+service nginx restart
+cp /vagrant/files/20-xdebug.ini /etc/php5/fpm/conf.d/20-xdebug.ini
+service nginx restart
+/usr/local/bin/composer self-update
+echo "" >> /home/vagrant/.bashrc
+echo "PATH=$PATH:/home/vagrant/svpercrm/vendor/bin" >> /home/vagrant/.bashrc
+echo "export PATH" >> /home/vagrant/.bashrc
