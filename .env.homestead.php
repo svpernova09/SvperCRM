d0e4c9c88d165937b1af0dad353f209bf31c8346
Svpernova09 <joe@joeferguson.me>
2014-09-20 13:42:04 -0500

    Adding env files

---
+++ .env.homestead.php
@@ -0,0 +1,8 @@
+<?php return array (
+    'DB_HOST' => 'localhost',
+    'DB_NAME' => 'homestead',
+    'DB_USERNAME' => 'homestead',
+    'DB_PASSWORD' => 'secret',
+    'MANDRILL_SECRET' => 'SNkmqZuORMYZr4y71dW8Iw',
+    'DEBUG' => true
+);
---
+++ .env.testing.php
@@ -0,0 +1,8 @@
+<?php return array (
+    'DB_HOST' => 'localhost',
+    'DB_NAME' => 'homestead',
+    'DB_USERNAME' => 'homestead',
+    'DB_PASSWORD' => 'secret',
+    'MANDRILL_SECRET' => '',
+    'DEBUG' => true
+);
