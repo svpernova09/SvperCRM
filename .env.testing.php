e9a1e4fdf5fc391134c5215151b866e47c5b027b
Svpernova09 <joe@joeferguson.me>
2014-09-20 14:19:35 -0500

    setting up testing env for travis

--- .env.testing.php
+++ .env.testing.php
@@ -1,8 +1,8 @@
 <?php return array (
     'DB_HOST' => 'localhost',
     'DB_NAME' => 'homestead',
-    'DB_USERNAME' => 'homestead',
-    'DB_PASSWORD' => 'secret',
+    'DB_USERNAME' => 'travis',
+    'DB_PASSWORD' => '',
     'MANDRILL_SECRET' => '',
     'DEBUG' => true
 );
